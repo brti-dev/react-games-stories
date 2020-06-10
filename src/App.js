1729fb41f58ee6c73ab9454805695c7097cc7ca8
dr-spaceman <tao@wangweitaodeMacBook-Air.local>
2020-06-10 16:32:05 +0800

    Imperative React: useRef hook to reference an element and autoFocus on it

--- src/App.js
+++ src/App.js
@@ -114,7 +114,7 @@ const App = () => {
       </p>
 
       {/* prop.children = 'Search' */}
-      <InputWithLabel id="search" value={searchTerm} onInputChange={handleSearch} numResults={searchNum}>
+      <InputWithLabel id="search" value={searchTerm} onInputChange={handleSearch} numResults={searchNum} isFocused>
         Search: 
       </InputWithLabel>
 
@@ -132,19 +132,36 @@ const App = () => {
  * @param {Event} onInputChange
  * @param {Number} numResults Number of results after list is filtered
  * @param {String} children Inner HTML of the component
+ * @param {Boolean} isFocused Reference to the input field's focus
  */
 function InputWithLabel(props) {
   console.log('InputWithLabel component', 'props:', props)
 
-  const {id, type='text', children, value, onInputChange, numResults} = props
+  const {id, type='text', children, value, onInputChange, numResults, isFocused} = props
+  
+  // Create a reference to an element
+  // This will later be assigned to the text input element so we can reference it elsewhere in the component
+  // Reference is persistent value for lifetime of component
+  const inputRef = React.useRef()
+
+  // Opt into React’s lifecycle with useEffect Hook
+  // Focus on input field when the component renders (or its dependencies change)
+  React.useEffect(() => {
+    console.log('useEffect:isFocused', isFocused, inputRef)
+    // Access the ref.current property, a mounted text input element
+    if (isFocused && inputRef.current) {
+      inputRef.current.focus()
+    }
+  }, [isFocused])
 
   return (
     <>
       <fieldset>
         <label htmlFor={id}>{children}</label>
-        {/* callback directly to props 
-            By adding React value, it becomes a controlled component */}
-        <input id={id} type={type} value={value} onChange={onInputChange} />
+        {/* Callback directly to props 
+            By adding React value, it becomes a controlled component
+            Access the reserved ref field */}
+        <input ref={inputRef} id={id} type={type} value={value} onChange={onInputChange} />
       </fieldset>
 
       <p>Searching for <em>{value}</em> | <b>{numResults || 'No'}</b> results</p>
